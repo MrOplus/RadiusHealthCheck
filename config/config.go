d83a2f78a5bdf8a164866bfa1823d14052229c77
Kooroshh <mr.oplus@yahoo.fr>
2022-01-14 21:57:41 +0330

    docker-access

--- config/config.go
+++ config/config.go
@@ -3,11 +3,16 @@ package config
 import "github.com/kooroshh/RadiusHealthCheck/models"
 
 type Config struct {
-	Servers []models.Server `json:"Servers"`
+	Servers     []models.Server         `json:"Servers"`
 	Credentials models.BasicCredentials `json:"Credentials"`
-	Interval int `json:"Interval"`
-	Hook     struct {
-		Url         string `json:"Url"`
+	Interval    int                     `json:"Interval"`
+	Hook        struct {
+		Enabled     bool                    `json:"Enabled"`
+		Url         string                  `json:"Url"`
 		Credentials models.BasicCredentials `json:"Credentials"`
 	} `json:"Hook"`
+	ContainerControl struct {
+		Enabled       bool   `json:"Enabled"`
+		ContainerName string `json:"ContainerName"`
+	} `json:"ContainerControl"`
 }
--- healthcheck/check.go
+++ healthcheck/check.go
@@ -8,9 +8,11 @@ import (
 	"github.com/kooroshh/RadiusHealthCheck/models"
 	"layeh.com/radius"
 	"layeh.com/radius/rfc2865"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"strings"
 	"time"
 )
@@ -21,6 +23,8 @@ func StartHealthCheck(server *models.Server, config *config.Config) {
 		packet := radius.New(radius.CodeAccessRequest, []byte(server.Secret))
 		_ = rfc2865.UserName_SetString(packet, config.Credentials.Username)
 		_ = rfc2865.UserPassword_SetString(packet, config.Credentials.Password)
+		_ = rfc2865.NASIPAddress_Set(packet, net.IPv4(127, 0, 0, 1)) //for some reason it doesn't work without this
+		_ = rfc2865.NASPort_Set(packet, 2000)                        //for some reason it doesn't work without this
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 		defer cancel()
 		response, err := radius.Exchange(ctx, packet, fmt.Sprintf("%s:%d", server.Address, server.Port))
@@ -34,38 +38,54 @@ func StartHealthCheck(server *models.Server, config *config.Config) {
 		} else {
 			if response.Code != radius.CodeAccessAccept {
 				fails++
+			} else if response.Code == radius.CodeAccessAccept {
+				fmt.Fprintf(os.Stdout, "Server %s is up\n", server.Address)
 			}
+			fmt.Fprintf(os.Stderr, "Address = %s,Code = %d\n", server.Address, response.Code)
 		}
 		if fails >= server.TriggerCount {
 			if callTheAmbulance(server.Address, config) != nil {
-				return
+				fmt.Fprintf(os.Stderr, "Unable to call the ambulance\n")
+			} else {
+				fails = 0
 			}
-			fails = 0
 		}
 		time.Sleep(time.Duration(config.Interval) * time.Second)
 	}
 }
 
 func callTheAmbulance(address string, config *config.Config) error {
-	data := url.Values{}
-	data.Add("ServerAddress", address)
-	data.Add("Time", time.Now().String())
+	if config.Hook.Enabled {
+		data := url.Values{}
+		data.Add("ServerAddress", address)
+		data.Add("Time", time.Now().String())
 
-	client := http.Client{}
+		client := http.Client{}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-	req, err := http.NewRequestWithContext(ctx, "POST", config.Hook.Url, strings.NewReader(data.Encode()))
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	if !models.IsBasicCredentialsEmpty(config.Hook.Credentials) {
-		req.SetBasicAuth(config.Hook.Credentials.Username, config.Hook.Credentials.Password)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		req, err := http.NewRequestWithContext(ctx, "POST", config.Hook.Url, strings.NewReader(data.Encode()))
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		if !models.IsBasicCredentialsEmpty(config.Hook.Credentials) {
+			req.SetBasicAuth(config.Hook.Credentials.Username, config.Hook.Credentials.Password)
+		}
+		if err != nil {
+			return err
+		}
+		_, err = client.Do(req)
+		if err != nil {
+			return err
+		}
 	}
-	if err != nil {
-		return err
-	}
-	_, err = client.Do(req)
-	if err != nil {
-		return err
+	if config.ContainerControl.Enabled {
+		cmd := exec.Command("/usr/bin/docker", "restart", config.ContainerControl.ContainerName)
+		stdout, err := cmd.Output()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to restart the container : %s\n", err.Error())
+			return err
+		}
+		time.Sleep(10 * time.Second)
+		fmt.Printf("%s\n", stdout)
 	}
 	return nil
 }
